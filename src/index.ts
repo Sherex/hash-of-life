b34d40f5890a8e38c51cd62825fc22ede8d5202a
Ingar Helgesen <ingar@i-h.no>
2021-04-02 10:17:02 +0200

    Add basic grid stuff

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,21 @@ interface GameOfLifeRule {
 
 interface GameOfLifeOptions {
   rule: string
-  board: number[][]
+  grid: number[][]
 }
+
+type GridCell = number | string
+
+function isGridCell (cell: any): cell is GridCell {
+  return typeof cell === 'number'
+}
+
+interface Position {
+  x: number
+  y: number
+}
+
+type LoopCellsCallback = (cell: GridCell, position: Position) => void
 
 export function parseGameOfLifeRule (str: string): GameOfLifeRule {
   const ruleRegex = /^B(?<born>[0-8]{0,8})\/S(?<survival>[0-8]{0,8})$/i
@@ -29,16 +42,53 @@ export function parseGameOfLifeRule (str: string): GameOfLifeRule {
 
 export class GameOfLife {
   rule: GameOfLifeRule
-  board: number[][]
+  grid: GridCell[][]
   constructor (options: GameOfLifeOptions) {
     this.rule = parseGameOfLifeRule(options.rule)
-    this.board = options.board
+    this.grid = options.grid
+  }
+
+  loopCells (eachRowCb: LoopCellsCallback): void {
+    this.grid.forEach((row, y) => {
+      row.forEach((cell, x) => {
+        eachRowCb(cell, { x: x, y: y })
+      })
+    })
+  }
+
+  getCell ({ x, y }: Position): GridCell | undefined {
+    return this.grid[y]?.[x]
+  }
+
+  getNeighbours ({ x, y }: Position): GridCell[] {
+    const neighbours = [
+      { x: x - 1, y: y + 1 },
+      { x: x, y: y + 1 },
+      { x: x + 1, y: y + 1 },
+      { x: x + 1, y: y },
+      { x: x + 1, y: y - 1 },
+      { x: x, y: y - 1 },
+      { x: x - 1, y: y - 1 },
+      { x: x - 1, y: y }
+    ]
+    return neighbours
+      .map(cell => this.getCell({ x: cell.x, y: cell.y }))
+      .filter(isGridCell)
+  }
+
+  printGrid (): void {
+    const printBoard: string[] = []
+    this.grid.forEach(row => {
+      const rowStrings = row.map(cell => cell === 1 ? '#' : '-')
+      printBoard.push(rowStrings.join(' '))
+    })
+    console.log(printBoard.join('\n'))
   }
 }
 
 const gol = new GameOfLife({
   rule: 'B3/S23',
-  board: [
+  grid: [
     [0, 0, 0, 0, 0],
     [0, 0, 1, 0, 0],
     [0, 0, 1, 0, 0],
@@ -47,4 +97,5 @@ const gol = new GameOfLife({
   ]
 })
 
-console.log(gol.rule)
+console.log(gol.getNeighbours({ x: 2, y: 2 }))
+gol.printGrid()
